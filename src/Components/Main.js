5dc302a8295e2fe67da04c500deda8f64a827894
Sudipta Saha Niloy <niloy305968@gmail.com>
2021-08-25 13:30:53 +0600

    initializing new react version of the app

---
+++ src/Components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Mainbody from './Body/Mainbody';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+
+function Main() {
+    return (
+        <div>
+            <Header />
+            <Mainbody />
+            <Footer />
+        </div>
+    )
+}
+
+export default Main
