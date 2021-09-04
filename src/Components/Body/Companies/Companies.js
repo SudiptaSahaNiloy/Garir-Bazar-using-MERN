3dc486f2701d8ba5732786f07caf05d5d02293f7
Sudipta Saha Niloy <niloy305968@gmail.com>
2021-09-04 22:41:35 +0600

    changed the company file

--- src/Components/Body/Companies/Companies.js
+++ src/Components/Body/Companies/Companies.js
@@ -8,37 +8,31 @@ function Companies() {
 
   return (
     <Carousel>
-  <Carousel.Item interval={1000}>
-    <img
-      className="d-block w-100"
-      src="https://wallpapercave.com/wp/wp1861755.jpg"
-      alt="Audi"
-    />
-    <Carousel.Caption className="Caption" >
-            <h1>WELCOME TO OUR AUDI COLLECTION</h1>
-            <p>Lets take a ride</p>
-            <Button href="/audiHome" className="button" variant="primary" size="lg">Ride With AUDI</Button>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={1000}>
-    <img
-      className="d-block w-100"
-      src="https://wallpaperaccess.com/full/1867214.jpg"
-      alt="Toyota"
-    />
-    <Carousel.Caption className="Caption">
-            <h1>WELCOME TO OUR TOYOTA COLLECTION</h1>
-            <p>Lets take a ride</p>
-            <Button href="/toyotaHome" className="button" variant="primary" size="lg">Ride With Toyota</Button>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-
-
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src="Images/audiHome.webp"
+          alt="Audi"
+        />
+        <Carousel.Caption className="Caption" >
+          <h1>WELCOME TO OUR AUDI COLLECTION</h1>
+          <p>Lets take a ride</p>
+          <Button href="/audiHome" className="button" variant="primary" size="lg">Ride With AUDI</Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src="Images/toyotaHome.jpg"
+          alt="Toyota"
+        />
+        <Carousel.Caption className="Caption">
+          <h1>WELCOME TO OUR TOYOTA COLLECTION</h1>
+          <p>Lets take a ride</p>
+          <Button href="/toyotaHome" className="button" variant="primary" size="lg">Ride With Toyota</Button>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   )
-
 }
-
- 
-
 export default Companies;
