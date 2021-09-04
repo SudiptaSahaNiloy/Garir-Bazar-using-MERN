d61ad479ea002ab0c88a4f1a78dbda2c50056d41
Beasty356 <zobairf03@gmail.com>
2021-09-04 22:05:02 +0600

    Companies

--- src/Components/Body/Companies/Companies.js
+++ src/Components/Body/Companies/Companies.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import './Stylesheet/Companies.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Stylesheet/Companies.css'
+import Button from 'react-bootstrap/Button' 
+import Carousel from 'react-bootstrap/Carousel'
+
+
+
 
 function Companies() {
     return (
-        <div>
-            AUdi
-        </div>
+
+    <Carousel>
+  <Carousel.Item interval={1000}>
+    <img
+      className="d-block w-100"
+      src="https://wallpapercave.com/wp/wp1861755.jpg"
+      alt="Audi"
+    />
+    <Carousel.Caption className="Caption" >
+            <h1>WELCOME TO OUR AUDI COLLECTION</h1>
+            <p>Lets take a ride</p>
+            <Button href="#" className="button" variant="primary" size="lg">Ride With AUDI</Button>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={1000}>
+    <img
+      className="d-block w-100"
+      src="https://wallpapercave.com/wp/wp8503921.jpg"
+      alt="Toyota"
+    />
+    <Carousel.Caption className="Caption">
+            <h1>WELCOME TO OUR TOYOTA COLLECTION</h1>
+            <p>Lets take a ride</p>
+            <Button href="#" className="button" variant="primary" size="lg">Ride With Toyota</Button>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
+
+        
     )
 }
 
